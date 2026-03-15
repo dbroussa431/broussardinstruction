66f6e4da43dc5cb932e111e1710380b39d507887
dbroussa431 <broussardshootingacademy@protonmail.com>
2026-03-14 22:32:55 -0500

    Update firebase-config.js

--- training/firebase-config.js
+++ training/firebase-config.js
@@ -1,18 +1,17 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Firebase configuration
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCF3y6cr25Ls9MKM2YPJLHJEAgA3PzRp-o",
+  authDomain: "bsa-training-admin.firebaseapp.com",
+  projectId: "bsa-training-admin",
+  storageBucket: "bsa-training-admin.firebasestorage.app",
+  messagingSenderId: "972595893407",
+  appId: "1:972595893407:web:f415a0f24602bd9603beaa"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-export { db };
