3efbb15f0d5f8e40fe78c97c855e7314eae25cf4
dbroussa431 <broussardshootingacademy@protonmail.com>
2026-03-15 21:52:11 -0500

    Create course-manifest.js

---
+++ training/js/course-manifest.js
@@ -0,0 +1,14 @@
+export const COURSE_MANIFEST = {
+  activeCourseVersion: "2026-03",
+  quizPullCount: 20,
+  scenarioPullCount: 2,
+  totalLessons: 8,
+  versions: {
+    "2026-03": {
+      label: "BSA Course March 2026",
+      sourceEdition: "BSA / USCCA aligned 2026-03",
+      lastReviewed: "2026-03-15",
+      lessons: [1, 2, 3, 4, 5, 6, 7, 8]
+    }
+  }
+};
