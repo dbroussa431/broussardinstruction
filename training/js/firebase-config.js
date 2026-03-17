7eefb55ded543c43b6a76107c965c7c1dd74081f
dbroussa431 <broussardshootingacademy@protonmail.com>
2026-03-17 14:55:10 -0500

    Update firebase-config.js

--- training/js/firebase-config.js
+++ training/js/firebase-config.js
@@ -1,19 +1,12 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+rules_version = '2';
+service cloud.firestore {
+  match /databases/{database}/documents {
+    match /portalStudents/{studentId} {
+      allow read, write: if true;
+    }
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCF3y6cr25Ls9MKM2YPJLHJEAgA3PzRp-o",
-  authDomain: "bsa-training-admin.firebaseapp.com",
-  projectId: "bsa-training-admin",
-  storageBucket: "bsa-training-admin.firebasestorage.app",
-  messagingSenderId: "972595893407",
-  appId: "1:972595893407:web:f415a0f24602bd9603beaa"
-};
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-export { app, auth, db };
-
+    match /students/{studentId} {
+      allow read, write: if true;
+    }
+  }
+}
