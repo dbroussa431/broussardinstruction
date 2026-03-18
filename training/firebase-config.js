0b4df30eeabab0486f66cc31b10bec732f87b0bd
dbroussa431 <broussardshootingacademy@protonmail.com>
2026-03-18 10:18:08 -0500

    Add files via upload

--- training/firebase-config.js
+++ training/firebase-config.js
@@ -5,6 +5,9 @@ const firebaseConfig = {
   apiKey: "AIzaSyCF3y6cr25Ls9MKM2YPJLHJEAgA3PzRp-o",
   authDomain: "bsa-training-admin.firebaseapp.com",
   projectId: "bsa-training-admin",
+  storageBucket: "bsa-training-admin.firebasestorage.app",
+  messagingSenderId: "972595893407",
+  appId: "1:972595893407:web:f415a0f24602bd9603beaa"
 };
 
 const app = initializeApp(firebaseConfig);
