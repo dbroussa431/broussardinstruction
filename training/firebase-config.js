cf3e199c35de18b30b589e1a02672cb734f5bd18
dbroussa431 <broussardshootingacademy@protonmail.com>
2026-03-18 08:19:02 -0500

    Update firebase-config.js

--- training/firebase-config.js
+++ training/firebase-config.js
@@ -1,18 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCF3y6cr25Ls9MKM2YPJLHJEAgA3PzRp-o",
   authDomain: "bsa-training-admin.firebaseapp.com",
   projectId: "bsa-training-admin",
-  storageBucket: "bsa-training-admin.firebasestorage.app",
-  messagingSenderId: "972595893407",
-  appId: "1:972595893407:web:f415a0f24602bd9603beaa"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-const auth = getAuth(app);
 
-export { app, db, auth };
+export { db };
