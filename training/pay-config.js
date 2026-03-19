3e978570a6366fc2ce793e17eceb047705defed9
dbroussa431 <broussardshootingacademy@protonmail.com>
2026-03-19 18:54:47 -0500

    Update pay-config.js

--- training/pay-config.js
+++ training/pay-config.js
@@ -1,11 +1,11 @@
 window.BSA_PAY_CONFIG = {
-  paypalClientId: "AXA3pfi5Xw9XYJCftXrScMroVnGMhmDfxbZkQL8BWm5n5y0Ivolgd9TMms7mfWEvSMDQUbka4Hz7A_rc",
+  paypalClientId: "AXA3pfi5Xw9XYJCftXrScMroVnGMhmDfxbZkQL8BWm5n5y0IvoIgd9TMms7mfWEvSMDQUbka4Hz7A_rc",
   courseName: "Louisiana Concealed Carry",
   coursePrice: 150,
   currency: "USD",
-  purchaseSuccessUrl: "https://broussardshootingacademy.com/training/login.html?purchase=success",
-  purchaseCancelUrl: "https://broussardshootingacademy.com/training/purchase.html?purchase=cancelled",
   functionsBaseUrl: "https://us-central1-bsa-training-admin.cloudfunctions.net",
   supportEmail: "BroussardShootingAcademy@protonmail.com",
-  supportPhone: "504-289-6605"
+  supportPhone: "504-289-6605",
+  loginUrl: "https://broussardshootingacademy.com/training/login.html",
+  purchaseUrl: "https://broussardshootingacademy.com/training/purchase.html"
 };
