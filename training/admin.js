09a14b637b206611f69b60b78853de79b82dc12a
dbroussa431 <broussardshootingacademy@protonmail.com>
2026-04-18 11:59:04 -0500

    Update admin.js

--- training/admin.js
+++ training/admin.js
@@ -575,6 +575,7 @@ function renderRows(rows) {
             <button class="btn btn-outline" data-action="edit" data-id="${s.id}" type="button">Edit</button>
             <button class="btn btn-outline" data-action="regen" data-id="${s.id}" type="button">Regen</button>
             <button class="btn ${isLocked ? "btn-light" : "btn-outline"}" data-action="toggle-lock" data-id="${s.id}" type="button">${isLocked ? "Unlock" : "Lock"}</button>
+            const isCompleted = getCourseStatus(s) === "Complete";
             <button class="btn btn-outline" data-action="log-email" data-id="${s.id}" type="button" ${loggableType ? "" : "disabled"}>${loggableType ? `Log ${loggableType}` : "No Email Due"}</button>
             <button class="btn btn-outline" data-action="delete" data-id="${s.id}" type="button">Delete</button>
           </div>
